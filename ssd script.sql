8c32e220e888a464e0b950391006abceee6c4a04
Chetan Patil <chetan.guru1008@gmail.com>
2017-05-28 04:45:55 -0700

    chages: new report related tables
    old table ddl are commented and a new report related table are created

--- ssd script.sql
+++ ssd script.sql
@@ -94,9 +94,12 @@ COLLATE = utf8_general_ci;
     
 	/* Bridge table for many2many mapping*/
 	CREATE TABLE `child_care`.`activity_vs_material` (
-    idmaterial INT UNSIGNED NOT NULL ,
-    idactivity INT UNSIGNED NOT NULL ,
-    primary key pk_activity2material(idmaterial,idactivity)
+    fk_idmaterial INT UNSIGNED NOT NULL ,
+    fk_idactivity INT UNSIGNED NOT NULL ,
+	CONSTRAINT activity_vs_material.fk_idmaterial FOREIGN KEY(fk_idmaterial)  REFERENCES child_care.material(idmaterial),
+    CONSTRAINT activity_vs_material.fk_idactivity FOREIGN KEY(fk_idactivity)  REFERENCES child_care.activity(idactivity),
+    
+    primary key pk_activity2material(fk_idmaterial,fk_idactivity)
    )ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8;  
 	
@@ -121,10 +124,12 @@ COLLATE = utf8_general_ci;
  /* Bridge table for many2many mapping*/
 8. create child_vs_careprovider
   
-    CREATE TABLE `child_care`.`child_vs_careprovider` (
-    idchild INT UNSIGNED NOT NULL ,
-    idcare_provider INT UNSIGNED NOT NULL ,
-    primary key pk_child2provider(idchild,idcare_provider)
+  CREATE TABLE `child_care`.`child_vs_careprovider` (
+    fk_idchild INT UNSIGNED NOT NULL ,
+    fk_idcare_provider INT UNSIGNED NOT NULL ,
+    CONSTRAINT child_vs_careprovider.fk_idchild FOREIGN KEY(fk_idchild) REFERENCES child_info(idchild),
+    CONSTRAINT child_vs_careprovider.fk_idcare_provider FOREIGN KEY(fk_idcare_provider) REFERENCES care_provider(idcare_provider),
+    primary key pk_child2provider(fk_idchild,fk_idcare_provider)
    )ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8
      COLLATE = utf8_general_ci;	
@@ -158,7 +163,7 @@ values('7:00am','DaycareOpen'),
 10.  create moring,afternoon,evening_activity plan which is accesible by parent for customisation?
 ANS: /*parent will have access to three activity tables morning,afetrnoon and evening to add activities for his child there is no time but consider activities are conducted 3 times a day*/
    
-   CREATE TABLE `child_care`.`morning_activity` (
+  /* CREATE TABLE `child_care`.`morning_activity` (
   `fk_idchild` INT UNSIGNED NOT NULL ,
   `MON` VARCHAR(50),
   `TUE` VARCHAR(50),
@@ -202,7 +207,7 @@ ANS:
 
 /*scores will be given out of 100*/
 
-CREATE TABLE `child_care`.`evening_activity_score` (
+/*CREATE TABLE `child_care`.`evening_activity_score` (
   `fk_idchild` INT UNSIGNED NOT NULL ,
   `MON` INT(3),
   `TUE` INT(3),
@@ -239,7 +244,46 @@ CREATE TABLE `child_care`.`morning_activity_score` (
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;*/
+
+
+
+11. Report table and day_session(morning,afternoon,evening)
+
+ANS:
+drop table if exists report;
+drop table if exists day_session;
+
+CREATE TABLE child_care.day_session(
+idsession INT(2) UNSIGNED NOT NULL UNIQUE,
+session_name VARCHAR(40),
+ PRIMARY KEY(idsession))
+ DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
+
+insert into child_care.day_session
+values(1,'Morning Session'),
+(2,'Afternoon Session'),
+(3,'Evening Session');
+ 
+CREATE TABLE `child_care`.`report` (
+  fk_idchild INT unsigned NOT NULL ,
+  fk_idactivity INT unsigned NOT NULL,
+  fk_idsession  INT unsigned  NOT NULL,
+  `MON` INT(3) NULL,
+  `TUE` INT(3) NULL,
+  `WEN` INT(3) NULL,
+  `THU` INT(3) NULL,
+  `FRI` INT(3) NULL,
+  CONSTRAINT report.fk_idsession FOREIGN KEY(fk_idsession)  REFERENCES child_care.day_session(idsession),
+  FOREIGN KEY report(fk_idchild) REFERENCES child_info(idchild) ON DELETE CASCADE,
+  FOREIGN KEY report(fk_idactivity) REFERENCES activity(idactivity) ON DELETE CASCADE,
+  PRIMARY KEY(fk_idchild,fk_idactivity)
+  )ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+
 
 
 
