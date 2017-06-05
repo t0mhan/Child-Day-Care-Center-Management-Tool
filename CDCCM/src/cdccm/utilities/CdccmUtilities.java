cb509783f1ae55574489a496c949319d4cb85652
Chetan Patil <chetan.guru1008@gmail.com>
2017-06-05 03:23:47 -0700

    age utility added

--- CDCCM/src/cdccm/utilities/CdccmUtilities.java
+++ CDCCM/src/cdccm/utilities/CdccmUtilities.java
@@ -2,9 +2,15 @@ package cdccm.utilities;
 
 public class CdccmUtilities {
 	
-	public static int getAge() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	public static int getAge(String dateOfBirth) throws ParseException {
+		//Returns the age by providing DOB.
+		String[] dayMonthYear = dateOfBirth.split("/");
+		
+		LocalDate birthdate = new LocalDate(Integer.parseInt(dayMonthYear[2]), Integer.parseInt(dayMonthYear[1]),
+				Integer.parseInt(dayMonthYear[0])); // Birth year,month,date
+		LocalDate now = new LocalDate(); // Today's date
+		Years age = Years.yearsBetween(birthdate, now);
 
+		return age.getYears();
+	}
 }
